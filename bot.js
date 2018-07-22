8965faff73310ed1bca76ed457e6984b57ed2590
mohamedElmusaui <41437450+mohamedElmusaui@users.noreply.github.com>
2018-07-22 13:39:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2132,43 +2132,30 @@ var prefix = "-";
 });
 
 client.on('message', message => {
-	var prefix = '-';
-  if (message.content.startsWith(prefix + "id")) {
-  if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
-   message.guild.fetchInvites().then(invs => {
-      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-      var moment = require('moment');
-      var args = message.content.split(" ").slice(1);
-let user = message.mentions.users.first();
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-moment.locale('ar-TN');
-      var id = new  Discord.RichEmbed()
-    .setColor("#0a0909")
-    .setAuthor(message.author.username, message.author.avatarURL)
-.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
-.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
-.addField(': عدد الدعوات', inviteCount,false)
-.setFooter("Speed Bot")
-    message.channel.sendEmbed(id);
-})
-}
-     });
+   if (message.content.startsWith("-id")) {
+                if(!message.channel.guild) return message.reply('** This command only for servers**');
 
+               var mentionned = message.mentions.users.first();
+    var mentionavatar;
+      if(mentionned){
+          var mentionavatar = mentionned;
+      } else {
+          var mentionavatar = message.author;
+          
+      }
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+   .setThumbnail(`${mentionavatar.avatarURL}`)
+  .addField("Name:",`<@` + `${mentionavatar.id}` + `>`, true)
+  .addField('Discrim:',"#" +  `${mentionavatar.discriminator}`, true)
+   .addField("ID:", "**[" + `${mentionavatar.id}` + "]**", true)
+  .addField("Create At:", "**[" + `${mentionavatar.createdAt}` + "]**", true)
+     
+     
+  message.channel.sendEmbed(embed);
+  console.log('[id] Send By: ' + message.author.username)
+    }
+});
 
 client.on('message', message => {
     if (message.content.startsWith("-bans")) {
