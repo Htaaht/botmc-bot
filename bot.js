952074f94935fd89ecaee2c8abf758c57155a875
mohamedElmusaui <41437450+mohamedElmusaui@users.noreply.github.com>
2018-07-27 22:03:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2181,7 +2181,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
         let embed = new Discord.RichEmbed()
         .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
-        .setTitle(`:small_orange_diamond: اضغط هنا `)
+        .setTitle(`اضغط هنا | Click Here `)
         .setURL(`https://discordapp.com/oauth2/authorize?client_id=400489866573512705&permissions=8&scope=bot`)
         .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
      message.channel.sendEmbed(embed);
