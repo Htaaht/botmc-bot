8cf7e2f9f6b51fda92fc7f57768973d5c78fdb4a
mohamedElmusaui <41437450+mohamedElmusaui@users.noreply.github.com>
2018-08-07 13:28:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2415,7 +2415,24 @@ client.on('message', async message => {
         .setFooter(`Speed Bot.`)
                 .setTimestamp()
     message.channel.send(embed)      
-}})
+}});
+
+client.on('message', ra3d => {
+var prefix = "-";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'cc')) {
+    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
