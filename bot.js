a08cf9bc056da442fbc3321e3cbfdec5bf967f30
mohamedElmusaui <41437450+mohamedElmusaui@users.noreply.github.com>
2018-09-13 12:54:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1208,24 +1208,6 @@ msg.channel.send(embed).then(() => {
 });
 
 
-client.on('ready', function(){	
-    var ms = 40000 ;	
-    var setGame = ['New Version','New update','-help'];	
-    var i = -1;	
-    var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
-});
-
 
 
 
